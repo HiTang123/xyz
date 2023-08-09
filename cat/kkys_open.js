b51b27a5c71a62fc96f7f891186336c2982dafa9
cacifer <18839137652@163.com>
2023-08-09 10:42:14 +0000

    更新 'cat/kkys_open.js'

--- cat/kkys_open.js
+++ cat/kkys_open.js
@@ -1,4 +1,4 @@
-import { Crypto, dayjs, jinja2, _ } from './lib/cat.js';
+import { Crypto, dayjs, jinja2, _ } from 'https://agit.ai/cacifer/xyz/raw/branch/master/cat/lib/cat.js';
 
 let key = 'kkys';
 let url = 'https://api1.baibaipei.com:8899';
