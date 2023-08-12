a4d160cd7931d7aa7b27f10b7e06ddc154f30621
cacifer <18839137652@163.com>
2023-08-12 03:20:10 +0000

    更新 'cat/czzy_open.js'

--- cat/czzy_open.js
+++ cat/czzy_open.js
@@ -1,7 +1,7 @@
 import { Crypto, load, _ } from 'https://agit.ai/cacifer/xyz/raw/branch/master/cat/lib/cat.js';
 
 let key = 'czzy';
-let url = 'https://cz01.cc/';
+let url = 'https://cz01.cc';
 let siteKey = '';
 let siteType = 0;
 
