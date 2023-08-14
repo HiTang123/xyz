4a48c87bafaa40be30c84c6916e61708eb51b004
cacifer <18839137652@163.com>
2023-08-14 09:35:23 +0000

    上传文件至 'cat/main'

---
+++ cat/main/园艺栽培.js
@@ -0,0 +1,252 @@
+import { _ } from "assets://js/lib/cat.js";
+let key = '园艺栽培';
+let homeName = '园艺栽培';
+let HOST = 'https://api.bilibili.com';
+let siteKey = '';
+let siteType = 0;
+let searchable= 0;//searchable=1 启用搜索
+const PC_UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.361";
+// let cookie = "DedeUserID=690781341;DedeUserID__ckMd5=cabc96906269c5b6;SESSDATA=2245ba24%2C1684212125%2C466fd%2Ab2;bili_jct=de6fdb60c10f8a83910aa55d79407b4e;"; // 可更换成自己的cookie
+let cookie = "https://agit.ai/cacifer/xyz/raw/branch/master/main/cookie.txt"; // 可更换成自己的cookie
+
+async function request(reqUrl) {
+  const res = await req(reqUrl, {
+      headers: getMb(),
+  });
+  return res.content;
+}
+
+async function init(cfg) {
+  siteKey = cfg.skey;
+  siteType = cfg.stype;
+  if (cookie.startsWith('http')) cookie = await request(cookie);
+  // console.debug('我的哔哩 cookie =====>' + cookie); // js_debug.log
+}
+
+async function home(filter) {
+  let classes = [
+    {"type_name": "园艺","type_id": "园艺"},
+    {"type_name": "盆栽","type_id": "家庭盆栽推荐"},
+    {"type_name": "室内","type_id": "室内盆栽"},
+    {"type_name": "室外","type_id": "室外盆栽"},
+    {"type_name": "花卉","type_id": "花卉种植"},
+    {"type_name": "果树","type_id": "果树种植技术"},
+    {"type_name": "蔬菜","type_id": "蔬菜种植技术"},
+    {"type_name": "多肉","type_id": "多肉种植教程"},
+	{"type_name": "配土","type_id": "盆栽配土教程"},
+    {"type_name": "施肥","type_id": "盆栽肥料教程"},
+	{"type_name": "修剪","type_id": "盆栽修剪教程"},	
+	{"type_name": "嫁接","type_id": "盆栽嫁接教程"},
+    {"type_name": "鱼缸造景","type_id": "鱼缸造景"},
+    {"type_name": "芦丁鸡造景","type_id": "芦丁鸡造景"}
+];
+  let filterObj = {
+	  "园艺": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+	  "家庭盆栽推荐": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+      "室内盆栽": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+      "室外盆栽": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+      "花卉种植": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+      "果树种植技术": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+      "蔬菜种植技术": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+      "多肉种植教程": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+      "盆栽配土教程": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+      "盆栽肥料教程": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+	  "盆栽修剪教程": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+	  "盆栽嫁接教程": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+      "鱼缸造景": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+      "芦丁鸡造景": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}]
+};
+  return JSON.stringify({
+      class: classes,
+      filters: filterObj,
+  });
+}
+
+async function homeVod() {
+  let html = HOST + '/x/web-interface/search/type?search_type=video&keyword='+homeName;
+  let data = JSON.parse(await request(html)).data.result;
+  let videos = [];
+  data.forEach(function(it) {
+      videos.push({
+          vod_id: it.aid,
+          vod_name: stripHtmlTag(it.title),
+          vod_pic: 'http:'+it.pic,
+          vod_remarks: turnDHM(it.duration) || '',
+      });
+  });
+  return JSON.stringify({
+      list: videos,
+  });
+}
+
+async function category(tid, pg, filter, extend) {
+  let html = HOST + '/x/web-interface/search/type?search_type=video&page=' + pg + '&keyword=' + (extend.tid || tid) + '&duration=' + (extend.duration || '') + '&order=' + (extend.order || '');
+  let data = JSON.parse(await request(html)).data;
+  let videos = [];
+  data.result.forEach(function(it) {
+      videos.push({
+          vod_id: it.aid,
+          vod_name: stripHtmlTag(it.title),
+          vod_pic: 'https:' + it.pic,
+          vod_remarks: turnDHM(it.duration) || '',
+      });
+  });
+  return JSON.stringify({
+      page: parseInt(data.page),
+      pagecount: data.numPages,
+      limit: 20,
+      total: data.numResults,
+      list: videos,
+  });
+}
+
+async function detail(id) {
+  let data = JSON.parse(await request(HOST + '/x/web-interface/view?aid=' + id)).data;
+  let vod = {
+      vod_id: data.aid,
+      vod_name: stripHtmlTag(data.title),
+      vod_pic: data.pic,
+      type_name: data.tname,
+      vod_year: new Date(data.pubdate*1000).getFullYear(),
+      vod_remarks: data.duration || '',
+      vod_director: data.owner.name,
+      vod_content: stripHtmlTag(data.desc),
+  };
+  let episodes = data.pages;
+  let playurls = [];
+  episodes.forEach(function(it) {
+    let cid = it.cid;
+    let part = it.part.replace('#', '﹟').replace('$', '﹩');
+    playurls.push(
+        part + '$' + data.aid + '_' + cid
+    )
+  });
+  let playUrl = playurls.join('#');
+  vod.vod_play_from = 'B站';
+  vod.vod_play_url = playUrl;
+  return JSON.stringify({
+    list: [vod],
+  });
+}
+
+async function play(flag, id, flags) {
+  let ids = id.split('_');
+  let html = HOST + '/x/player/playurl?avid=' + ids[0] + '&cid=' + ids[1] + '&qn=116';
+  let data = JSON.parse(await request(html)).data.durl;
+  let maxSize = -1;
+  let position = -1;
+  data.forEach(function(it, i) {
+      if (maxSize < Number(it.size)) {
+          maxSize = Number(it.size);
+          position = i
+      }
+  });
+  let purl = '';
+  if (data.length > 0) {
+    if (position === -1) {
+        position = 0
+    }
+    purl = data[position].url
+  }
+  // console.debug('我的哔哩 purl =====>' + purl); // js_debug.log
+  return JSON.stringify({
+    parse: 0,
+    url: purl,
+    header: getMb(),
+  });
+}
+
+async function search(wd, quick, pg) {
+  if(searchable==1){
+  if (pg <= 0 || typeof(pg) == 'undefined') pg = 1;
+  let html = HOST + '/x/web-interface/search/type?search_type=video&keyword=' + wd + '&page=' + pg;
+  let data = JSON.parse(await request(html)).data;
+  let videos = [];
+  data.result.forEach(function(it) {
+    videos.push({
+        vod_id: it.aid,
+        vod_name: stripHtmlTag(it.title),
+        vod_pic: 'https:' + it.pic,
+        vod_remarks: turnDHM(it.duration) || '',
+    });
+  });
+  return JSON.stringify({
+      page: parseInt(data.page),
+      pagecount: data.numPages,
+      limit: 20,
+      total: data.numResults,
+      list: videos,
+  });
+  }else{
+	  return null;
+  }
+}
+
+function getHeader(cookie) {
+  let header = {};
+  header['cookie'] = cookie;
+  header['User-Agent'] = PC_UA;
+  header['Referer'] = 'https://www.bilibili.com';
+  return header;
+}
+
+function getMb() {
+  return getHeader(cookie);
+}
+
+function stripHtmlTag(src) {
+  return src
+      .replace(/<\/?[^>]+(>|$)/g, '')
+      .replace(/&.{1,5};/g, '')
+      .replace(/\s{2,}/g, ' ');
+}
+
+function turnDHM(duration) {
+  let min = duration.split(':')[0];
+  let sec = duration.split(':')[1];
+  if (min == 0) {
+      return sec + '秒';
+  } else if (0 < min && min < 60) {
+      return min + '分';
+  } else if (60 <= min && min < 1440) {
+      if (min % 60 == 0) {
+          let h = min / 60;
+          return h + '小时';
+      } else {
+          let h = min / 60;
+          h = (h + '').split('.')[0];
+          let m = min % 60;
+          return h + '小时' + m + '分';
+      }
+  } else if (min >= 1440) {
+      let d = min / 60 / 24;
+      d = (d + '').split('.')[0];
+      let h = min / 60 % 24;
+      h = (h + '').split('.')[0];
+      let m = min % 60;
+      let dhm = '';
+      if (d > 0) {
+          dhm = d + '天'
+      }
+      if (h >= 1) {
+          dhm = dhm + h + '小时'
+      }
+      if (m > 0) {
+          dhm = dhm + m + '分'
+      }
+      return dhm;
+  }
+  return null;
+}
+
+export function __jsEvalReturn() {
+  return {
+      init: init,
+      home: home,
+      homeVod: homeVod,
+      category: category,
+      detail: detail,
+      play: play,
+      search: search,
+  };
+}
