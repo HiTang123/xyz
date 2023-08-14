3d2192784aacf675893a4d7ca40d6372d343a97e
cacifer <18839137652@163.com>
2023-08-14 09:34:16 +0000

    上传文件至 'cat/main'

---
+++ cat/main/吹拉弹唱.js
@@ -0,0 +1,260 @@
+import { _ } from "assets://js/lib/cat.js";
+let key = '吹拉弹唱';
+let homeName = '乐器演奏';
+let HOST = 'https://api.bilibili.com';
+let siteKey = '';
+let siteType = 0;
+let searchable= 0;//searchable=1 启用搜索
+const PC_UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.361";
+// let cookie = "DedeUserID=690781341;DedeUserID__ckMd5=cabc96906269c5b6;SESSDATA=2245ba24%2C1684212125%2C466fd%2Ab2;bili_jct=de6fdb60c10f8a83910aa55d79407b4e;"; // 可更换成自己的cookie
+let cookie = "https://agit.ai/cacifer/xyz/raw/branch/master/main/cookie.txt"; // 可更换成自己的cookie
+
+async function request(reqUrl) {
+  const res = await req(reqUrl, {
+      headers: getMb(),
+  });
+  return res.content;
+}
+
+async function init(cfg) {
+  siteKey = cfg.skey;
+  siteType = cfg.stype;
+  if (cookie.startsWith('http')) cookie = await request(cookie);
+  // console.debug('我的哔哩 cookie =====>' + cookie); // js_debug.log
+}
+
+async function home(filter) {
+  let classes = [
+    {"type_name": "二胡","type_id": "二胡曲欣赏二胡教学"},
+    {"type_name": "古筝","type_id": "古筝曲欣赏古筝教学"},
+    {"type_name": "琵琶","type_id": "琵琶曲欣赏琵琶教学"},
+    {"type_name": "洞箫","type_id": "洞箫欣赏洞箫教学"},
+    {"type_name": "笛子","type_id": "笛子曲欣赏笛子教学"},
+    {"type_name": "唢呐","type_id": "唢呐名曲唢呐教学"},
+    {"type_name": "葫芦丝","type_id": "葫芦丝欣赏葫芦丝教学"},
+    {"type_name": "钢琴","type_id": "钢琴名曲欣赏钢琴教学"},
+    {"type_name": "口琴","type_id": "口琴欣赏口琴教学"},
+    {"type_name": "小提琴","type_id": "小提琴名曲欣赏小提琴教学"},
+    {"type_name": "吉他","type_id": "吉他欣赏吉他教程"},
+    {"type_name": "手风琴","type_id": "手风琴欣赏教程"},
+    {"type_name": "架子鼓","type_id": "架子鼓欣赏教程"},
+    {"type_name": "埙","type_id": "埙欣赏埙教学"},
+    {"type_name": "五线谱","type_id": "五线谱教程"},
+    {"type_name": "简谱","type_id": "简谱教程"},
+	{"type_name": "学唱歌","type_id": "声乐教程"}
+	
+];
+  let filterObj = {
+
+	  "二胡曲欣赏二胡教学": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+	  "古筝曲欣赏古筝教学": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+      "琵琶曲欣赏琵琶教学": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+      "洞箫欣赏洞箫教学": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+      "笛子曲欣赏笛子教学": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+      "唢呐名曲唢呐教学": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+      "葫芦丝欣赏葫芦丝教学": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+      "钢琴名曲欣赏钢琴教学": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+      "口琴欣赏口琴教学": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+      "小提琴名曲欣赏小提琴教学": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+      "吉他欣赏吉他教程": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+      "手风琴欣赏教程": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+      "架子鼓欣赏教程": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+	  "五线谱教程": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+	  "架子鼓欣赏教程": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+	  "声乐教程": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+      "简谱教程": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}]
+};
+  return JSON.stringify({
+      class: classes,
+      filters: filterObj,
+  });
+}
+
+async function homeVod() {
+  let html = HOST + '/x/web-interface/search/type?search_type=video&keyword='+homeName;
+  let data = JSON.parse(await request(html)).data.result;
+  let videos = [];
+  data.forEach(function(it) {
+      videos.push({
+          vod_id: it.aid,
+          vod_name: stripHtmlTag(it.title),
+          vod_pic: 'http:'+it.pic,
+          vod_remarks: turnDHM(it.duration) || '',
+      });
+  });
+  return JSON.stringify({
+      list: videos,
+  });
+}
+
+async function category(tid, pg, filter, extend) {
+  let html = HOST + '/x/web-interface/search/type?search_type=video&page=' + pg + '&keyword=' + (extend.tid || tid) + '&duration=' + (extend.duration || '') + '&order=' + (extend.order || '');
+  let data = JSON.parse(await request(html)).data;
+  let videos = [];
+  data.result.forEach(function(it) {
+      videos.push({
+          vod_id: it.aid,
+          vod_name: stripHtmlTag(it.title),
+          vod_pic: 'https:' + it.pic,
+          vod_remarks: turnDHM(it.duration) || '',
+      });
+  });
+  return JSON.stringify({
+      page: parseInt(data.page),
+      pagecount: data.numPages,
+      limit: 20,
+      total: data.numResults,
+      list: videos,
+  });
+}
+
+async function detail(id) {
+  let data = JSON.parse(await request(HOST + '/x/web-interface/view?aid=' + id)).data;
+  let vod = {
+      vod_id: data.aid,
+      vod_name: stripHtmlTag(data.title),
+      vod_pic: data.pic,
+      type_name: data.tname,
+      vod_year: new Date(data.pubdate*1000).getFullYear(),
+      vod_remarks: data.duration || '',
+      vod_director: data.owner.name,
+      vod_content: stripHtmlTag(data.desc),
+  };
+  let episodes = data.pages;
+  let playurls = [];
+  episodes.forEach(function(it) {
+    let cid = it.cid;
+    let part = it.part.replace('#', '﹟').replace('$', '﹩');
+    playurls.push(
+        part + '$' + data.aid + '_' + cid
+    )
+  });
+  let playUrl = playurls.join('#');
+  vod.vod_play_from = 'B站';
+  vod.vod_play_url = playUrl;
+  return JSON.stringify({
+    list: [vod],
+  });
+}
+
+async function play(flag, id, flags) {
+  let ids = id.split('_');
+  let html = HOST + '/x/player/playurl?avid=' + ids[0] + '&cid=' + ids[1] + '&qn=116';
+  let data = JSON.parse(await request(html)).data.durl;
+  let maxSize = -1;
+  let position = -1;
+  data.forEach(function(it, i) {
+      if (maxSize < Number(it.size)) {
+          maxSize = Number(it.size);
+          position = i
+      }
+  });
+  let purl = '';
+  if (data.length > 0) {
+    if (position === -1) {
+        position = 0
+    }
+    purl = data[position].url
+  }
+  // console.debug('我的哔哩 purl =====>' + purl); // js_debug.log
+  return JSON.stringify({
+    parse: 0,
+    url: purl,
+    header: getMb(),
+  });
+}
+
+async function search(wd, quick, pg) {
+  if(searchable==1){
+  if (pg <= 0 || typeof(pg) == 'undefined') pg = 1;
+  let html = HOST + '/x/web-interface/search/type?search_type=video&keyword=' + wd + '&page=' + pg;
+  let data = JSON.parse(await request(html)).data;
+  let videos = [];
+  data.result.forEach(function(it) {
+    videos.push({
+        vod_id: it.aid,
+        vod_name: stripHtmlTag(it.title),
+        vod_pic: 'https:' + it.pic,
+        vod_remarks: turnDHM(it.duration) || '',
+    });
+  });
+  return JSON.stringify({
+      page: parseInt(data.page),
+      pagecount: data.numPages,
+      limit: 20,
+      total: data.numResults,
+      list: videos,
+  });
+  }else{
+	  return null;
+  }
+}
+
+function getHeader(cookie) {
+  let header = {};
+  header['cookie'] = cookie;
+  header['User-Agent'] = PC_UA;
+  header['Referer'] = 'https://www.bilibili.com';
+  return header;
+}
+
+function getMb() {
+  return getHeader(cookie);
+}
+
+function stripHtmlTag(src) {
+  return src
+      .replace(/<\/?[^>]+(>|$)/g, '')
+      .replace(/&.{1,5};/g, '')
+      .replace(/\s{2,}/g, ' ');
+}
+
+function turnDHM(duration) {
+  let min = duration.split(':')[0];
+  let sec = duration.split(':')[1];
+  if (min == 0) {
+      return sec + '秒';
+  } else if (0 < min && min < 60) {
+      return min + '分';
+  } else if (60 <= min && min < 1440) {
+      if (min % 60 == 0) {
+          let h = min / 60;
+          return h + '小时';
+      } else {
+          let h = min / 60;
+          h = (h + '').split('.')[0];
+          let m = min % 60;
+          return h + '小时' + m + '分';
+      }
+  } else if (min >= 1440) {
+      let d = min / 60 / 24;
+      d = (d + '').split('.')[0];
+      let h = min / 60 % 24;
+      h = (h + '').split('.')[0];
+      let m = min % 60;
+      let dhm = '';
+      if (d > 0) {
+          dhm = d + '天'
+      }
+      if (h >= 1) {
+          dhm = dhm + h + '小时'
+      }
+      if (m > 0) {
+          dhm = dhm + m + '分'
+      }
+      return dhm;
+  }
+  return null;
+}
+
+export function __jsEvalReturn() {
+  return {
+      init: init,
+      home: home,
+      homeVod: homeVod,
+      category: category,
+      detail: detail,
+      play: play,
+      search: search,
+  };
+}
---
+++ cat/main/安全防骗.js
@@ -0,0 +1,239 @@
+import { _ } from "assets://js/lib/cat.js";
+let key = '安全防骗';
+let homeName = '网络安全防骗';
+let HOST = 'https://api.bilibili.com';
+let siteKey = '';
+let siteType = 0;
+let searchable=0;
+const PC_UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.361";
+// let cookie = "DedeUserID=690781341;DedeUserID__ckMd5=cabc96906269c5b6;SESSDATA=2245ba24%2C1684212125%2C466fd%2Ab2;bili_jct=de6fdb60c10f8a83910aa55d79407b4e;"; // 可更换成自己的cookie
+let cookie = "https://agit.ai/cacifer/xyz/raw/branch/master/main/cookie.txt"; // 可更换成自己的cookie
+
+async function request(reqUrl) {
+  const res = await req(reqUrl, {
+      headers: getMb(),
+  });
+  return res.content;
+}
+
+async function init(cfg) {
+  siteKey = cfg.skey;
+  siteType = cfg.stype;
+  if (cookie.startsWith('http')) cookie = await request(cookie);
+  // console.debug('我的哔哩 cookie =====>' + cookie); // js_debug.log
+}
+
+async function home(filter) {
+  let classes = [
+    {"type_name": "玩转手机","type_id": "教老人用手机"},
+    {"type_name": "玩转微信","type_id": "教老人使用微信"},
+    {"type_name": "骗局揭秘","type_id": "中老年人骗局揭秘"},
+	{"type_name": "赌博骗局","type_id": "赌博骗局"},	
+	{"type_name": "防电信诈骗","type_id": "防电信诈骗"},    
+    {"type_name": "防拐卖","type_id": "防拐卖妇女儿童"},
+    {"type_name": "防性侵","type_id": "女性防性侵"}
+];
+  let filterObj = {
+
+	  "教老人用手机": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+	  "教老人使用微信": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+      "中老年人骗局揭秘": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+      "防电信诈骗": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+      "防拐卖妇女儿童": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+      "女性防性侵": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+	  "赌博骗局": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}]
+};
+  return JSON.stringify({
+      class: classes,
+      filters: filterObj,
+  });
+}
+
+async function homeVod() {
+  let html = HOST + '/x/web-interface/search/type?search_type=video&keyword='+homeName;
+  let data = JSON.parse(await request(html)).data.result;
+  let videos = [];
+  data.forEach(function(it) {
+      videos.push({
+          vod_id: it.aid,
+          vod_name: stripHtmlTag(it.title),
+          vod_pic: 'http:'+it.pic,
+          vod_remarks: turnDHM(it.duration) || '',
+      });
+  });
+  return JSON.stringify({
+      list: videos,
+  });
+}
+
+async function category(tid, pg, filter, extend) {
+  let html = HOST + '/x/web-interface/search/type?search_type=video&page=' + pg + '&keyword=' + (extend.tid || tid) + '&duration=' + (extend.duration || '') + '&order=' + (extend.order || '');
+  let data = JSON.parse(await request(html)).data;
+  let videos = [];
+  data.result.forEach(function(it) {
+      videos.push({
+          vod_id: it.aid,
+          vod_name: stripHtmlTag(it.title),
+          vod_pic: 'https:' + it.pic,
+          vod_remarks: turnDHM(it.duration) || '',
+      });
+  });
+  return JSON.stringify({
+      page: parseInt(data.page),
+      pagecount: data.numPages,
+      limit: 20,
+      total: data.numResults,
+      list: videos,
+  });
+}
+
+async function detail(id) {
+  let data = JSON.parse(await request(HOST + '/x/web-interface/view?aid=' + id)).data;
+  let vod = {
+      vod_id: data.aid,
+      vod_name: stripHtmlTag(data.title),
+      vod_pic: data.pic,
+      type_name: data.tname,
+      vod_year: new Date(data.pubdate*1000).getFullYear(),
+      vod_remarks: data.duration || '',
+      vod_director: data.owner.name,
+      vod_content: stripHtmlTag(data.desc),
+  };
+  let episodes = data.pages;
+  let playurls = [];
+  episodes.forEach(function(it) {
+    let cid = it.cid;
+    let part = it.part.replace('#', '﹟').replace('$', '﹩');
+    playurls.push(
+        part + '$' + data.aid + '_' + cid
+    )
+  });
+  let playUrl = playurls.join('#');
+  vod.vod_play_from = 'B站';
+  vod.vod_play_url = playUrl;
+  return JSON.stringify({
+    list: [vod],
+  });
+}
+
+async function play(flag, id, flags) {
+  let ids = id.split('_');
+  let html = HOST + '/x/player/playurl?avid=' + ids[0] + '&cid=' + ids[1] + '&qn=116';
+  let data = JSON.parse(await request(html)).data.durl;
+  let maxSize = -1;
+  let position = -1;
+  data.forEach(function(it, i) {
+      if (maxSize < Number(it.size)) {
+          maxSize = Number(it.size);
+          position = i
+      }
+  });
+  let purl = '';
+  if (data.length > 0) {
+    if (position === -1) {
+        position = 0
+    }
+    purl = data[position].url
+  }
+  // console.debug('我的哔哩 purl =====>' + purl); // js_debug.log
+  return JSON.stringify({
+    parse: 0,
+    url: purl,
+    header: getMb(),
+  });
+}
+
+async function search(wd, quick, pg) {
+	if(searchable==1){
+  if (pg <= 0 || typeof(pg) == 'undefined') pg = 1;
+  let html = HOST + '/x/web-interface/search/type?search_type=video&keyword=' + wd + '&page=' + pg;
+  let data = JSON.parse(await request(html)).data;
+  let videos = [];
+  data.result.forEach(function(it) {
+    videos.push({
+        vod_id: it.aid,
+        vod_name: stripHtmlTag(it.title),
+        vod_pic: 'https:' + it.pic,
+        vod_remarks: turnDHM(it.duration) || '',
+    });
+  });
+  return JSON.stringify({
+      page: parseInt(data.page),
+      pagecount: data.numPages,
+      limit: 20,
+      total: data.numResults,
+      list: videos,
+  });
+	}else{
+		return null;
+	}
+}
+
+function getHeader(cookie) {
+  let header = {};
+  header['cookie'] = cookie;
+  header['User-Agent'] = PC_UA;
+  header['Referer'] = 'https://www.bilibili.com';
+  return header;
+}
+
+function getMb() {
+  return getHeader(cookie);
+}
+
+function stripHtmlTag(src) {
+  return src
+      .replace(/<\/?[^>]+(>|$)/g, '')
+      .replace(/&.{1,5};/g, '')
+      .replace(/\s{2,}/g, ' ');
+}
+
+function turnDHM(duration) {
+  let min = duration.split(':')[0];
+  let sec = duration.split(':')[1];
+  if (min == 0) {
+      return sec + '秒';
+  } else if (0 < min && min < 60) {
+      return min + '分';
+  } else if (60 <= min && min < 1440) {
+      if (min % 60 == 0) {
+          let h = min / 60;
+          return h + '小时';
+      } else {
+          let h = min / 60;
+          h = (h + '').split('.')[0];
+          let m = min % 60;
+          return h + '小时' + m + '分';
+      }
+  } else if (min >= 1440) {
+      let d = min / 60 / 24;
+      d = (d + '').split('.')[0];
+      let h = min / 60 % 24;
+      h = (h + '').split('.')[0];
+      let m = min % 60;
+      let dhm = '';
+      if (d > 0) {
+          dhm = d + '天'
+      }
+      if (h >= 1) {
+          dhm = dhm + h + '小时'
+      }
+      if (m > 0) {
+          dhm = dhm + m + '分'
+      }
+      return dhm;
+  }
+  return null;
+}
+
+export function __jsEvalReturn() {
+  return {
+      init: init,
+      home: home,
+      homeVod: homeVod,
+      category: category,
+      detail: detail,
+      play: play,
+      search: search,
+  };
+}
