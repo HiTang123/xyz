8e934d391e34a751d46ea6113693f54fdb0dd4b5
cacifer <18839137652@163.com>
2023-09-05 08:36:13 +0000

    更新 'JS/bili_diy.js'

--- JS/bili_diy.js
+++ JS/bili_diy.js
@@ -1,6 +1,6 @@
 import {_} from "assets://js/lib/cat.js";
 import {def_conf} from "./def_biliconf.js";
-import {配置} from "http://127.0.0.1:9978/file/tvbox/%E9%85%8D%E7%BD%AE_Bili%E6%BA%90.js";
+import {config} from "http://127.0.0.1:9978/file/tvbox/%E9%85%8D%E7%BD%AE_Bili%E6%BA%90.js";
 let key = 'bili_diy';
 let HOST = 'https://api.bilibili.com';
 let siteKey = '';
@@ -18,12 +18,12 @@ if(config.启用本地配置 != 1) {
 	}
 } else {
 	diy_conf = {
-		cookie: 配置.cookie,
-		searchable: 配置.搜索功能,
-		homeSwitch: 配置.显示推荐页,
-		homeName: 配置.推荐页显示内容,
-		classes: 配置.自定义分类,
-		filterObj: 配置.自定义筛选
+		cookie: config.cookie,
+		searchable: config.搜索功能,
+		homeSwitch: config.显示推荐页,
+		homeName: config.推荐页显示内容,
+		classes: config.自定义分类,
+		filterObj: config.自定义筛选
 	}
 };
 let cookie = diy_conf.cookie;
