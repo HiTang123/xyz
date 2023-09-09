da119290f34cc55af9dcbb70fcba7f46022c88d7
cacifer <18839137652@163.com>
2023-09-09 09:44:20 +0000

    更新 'JS/JustLive2.js'

--- JS/JustLive2.js
+++ JS/JustLive2.js
@@ -64,7 +64,7 @@ var rule = {
         });
         d.push({
 	                title: "斗鱼解析",
-	                url: "http://159.75.85.63:35455/douyu/" + jo.roomId
+	                url: "https://www.aois.eu.org/live/douyu/" + jo.roomId
                 });
         VOD.vod_play_from = "选择画质";
         VOD.vod_play_url = d.map(function(it) {
