f0269b3cd3d20111e77ccc986e7c515577d229a2
cacifer <18839137652@163.com>
2023-10-03 08:38:15 +0000

    更新 'cat/bookan_open.js'

--- cat/bookan_open.js
+++ cat/bookan_open.js
@@ -1,4 +1,4 @@
-import { _ } from 'https://agit.ai/cacifer/xyz/raw/branch/master/cat/lib/cat1.js';
+import { _ } from 'https://agit.ai/cacifer/xyz/raw/branch/master/cat/lib/cat.js';
 
 let key = 'bookan';
 let url = 'https://api.bookan.com.cn';
