ce7a56358a16cadbfd7afd44df049859f23353d6
cacifer <18839137652@163.com>
2023-10-05 07:19:17 +0000

    更新 'cat/czzy_open.js'

--- cat/czzy_open.js
+++ cat/czzy_open.js
@@ -3,8 +3,7 @@ import { Crypto, load, _ } from './lib/cat.js';
 let key = 'czzy';
 //发布页 https://cz01.vip
 //let url = 'https://cz01.cc';
-//let url = 'https://www.cz0101.com';
-let url = 'https://cz01.vip';
+let url = 'https://www.cz0101.com';
 let siteKey = '';
 let siteType = 0;
 
