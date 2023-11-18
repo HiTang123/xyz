778236616904f0692cc542440211ab3e2b3e4a9a
Cacifer <jack.cacifer@gmail.com>
2023-11-18 08:57:14 +0800

    update

---
+++ JS/量子资源.js
@@ -0,0 +1,87 @@
+var rule = {
+    title: '量子资源',
+    host: 'https://cj.lzcaiji.com',
+    homeUrl: '/api.php/provide/vod/from/ffm3u8?ac=detail',
+    searchUrl: '/api.php/provide/vod/from/ffm3u8?ac=detail&wd=**&pg=fypage',
+    detailUrl: '/api.php/provide/vod/from/ffm3u8?ac=detail&ids=fyid', //非必填,二级详情拼接链接
+    searchable: 2,
+    quickSearch: 0,
+    filterable: 1,
+    play_parse: false,
+    lazy: '',
+    multi: 1,
+    timeout: 5000,
+    limit: 6,
+    url: '/api.php/provide/vod?ac=detail&t=fyclass&pg=fypage&f=',
+    class_name: '短剧&国产剧&欧美剧&韩剧&动作片&科幻片&恐怖片&战争片&剧情片&喜剧片&爱情片&国产动漫&欧美动漫&日韩动漫',
+    class_url: '46&13&16&15&6&9&10&11&12&7&8&29&31&30',
+    推荐: 'json:list;vod_name;vod_pic;vod_remarks;vod_id', // double: true, // 推荐内容是否双层定位
+    // 一级: 'json:list;vod_name;vod_pic;vod_remarks;vod_id',
+    一级: `js:
+        function getParam(url,name) {
+            var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+            var r = url.split('?')[1].match(reg); //获取url中"?"符后的字符串并正则匹配
+            var context = "";
+            if (r != null)
+                context = decodeURIComponent(r[2]);
+            reg = null;
+            r = null;
+            return context == null || context == "" || context == "undefined" ? "" : context;
+        }
+        let d = [];
+        // 忽略分类
+        let cate_exclude = '34,35,45';
+        let type_id = getParam(input,'t');
+        if(!cate_exclude.match(type_id)){
+            let html = request(input);
+            let list = JSON.parse(html).list;
+            list.forEach(function (it){
+                if(!cate_exclude.match(it.type_id)){
+                    d.push({
+                        title:it.vod_name,
+                        img:it.vod_pic,
+                        desc:it.vod_remarks,
+                        url:it.vod_id
+                    });
+                }
+            });
+        }
+        setResult(d);
+        // log(input);
+    `,
+    /**
+     * 资源采集站，二级链接解析
+     */
+    // 二级: `json:list;vod_name;vod_pic;vod_remarks;vod_id`,
+    二级: `js:
+        let html = request(input);
+        let list = JSON.parse(html).list;
+        if(list.length===1){
+           VOD = list[0];
+            VOD.vod_blurb = VOD.vod_blurb.replace(/　/g, '').replace(/<[^>]*>/g, '');
+            VOD.vod_content = VOD.vod_content.replace(/　/g, '').replace(/<[^>]*>/g, '');
+        }
+    `,
+    /**
+     * 搜索解析 过滤部分资源
+     */
+    // 搜索: 'json:list;vod_name;vod_pic;vod_remarks;vod_id',
+    搜索: `js:
+        let d = [];
+        // 忽略分类
+        let cate_exclude = '34,35,45';
+        let html = request(input);
+        let list = JSON.parse(html).list;
+        list.forEach(function (it){
+            if(!cate_exclude.match(it.type_id)){
+                d.push({
+                    title:it.vod_name,
+                    img:it.vod_pic,
+                    desc:it.vod_remarks,
+                    url:it.vod_id
+                });
+            }
+        });
+        setResult(d);
+    `,
+}
