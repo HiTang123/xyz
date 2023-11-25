c9ca864b6e27f4efd19f58829244c3c0c2a99325
Cacifer <jack.cacifer@gmail.com>
2023-11-25 16:55:36 +0800

    Create 体育运动.js

---
+++ cat/main/体育运动.js
@@ -0,0 +1,280 @@
+import { _ } from "assets://js/lib/cat.js";
+let key = '体育运动';
+let homeName = '体育运动';
+let HOST = 'https://api.bilibili.com';
+let siteKey = '';
+let siteType = 0;
+let searchable= 0;//searchable=1 启用搜索
+const PC_UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.361";
+let cookie = "https://mirror.ghproxy.com/https://raw.githubusercontent.com/HiTang123/xyz/master/cat/main/cookie.txt";
+async function request(reqUrl) {
+  const res = await req(reqUrl, {
+      headers: getMb(),
+  });
+  return res.content;
+}
+
+async function init(cfg) {
+  siteKey = cfg.skey;
+  siteType = cfg.stype;
+  if (cookie.startsWith('http')) cookie = await request(cookie);
+  // console.debug('我的哔哩 cookie =====>' + cookie); // js_debug.log
+}
+
+async function home(filter) {
+  let classes = [
+    {"type_name": "⚽足球","type_id": "足球教学"},
+    {"type_name": "🏀篮球","type_id": "篮球教学"},
+    {"type_name": "🏓乒乓球","type_id": "乒乓球教学"},
+    {"type_name": "🏸羽毛球","type_id": "羽毛球教学"},
+    {"type_name": "🎾网球","type_id": "网球教学"},
+	{"type_name": "🏐排球","type_id": "排球教学"},
+    {"type_name": "🎱台球","type_id": "台球教学"},
+    {"type_name": "🎯飞镖","type_id": "扔飞镖教学"},
+    {"type_name": "🥊拳击","type_id": "拳击教学"},
+    {"type_name": "🥋武术","type_id": "武术教学"},
+    {"type_name": "🎽跑步","type_id": "跑步技巧"},
+    {"type_name": "🪁放风筝","type_id": "放风筝技巧"},
+    {"type_name": "🎣钓鱼","type_id": "钓鱼教学"},
+    {"type_name": "🏊‍游泳","type_id": "游泳教学"},
+    {"type_name": "🎿滑雪","type_id": "滑雪教学"},
+    {"type_name": "⛸️滑冰","type_id": "滑冰教学"},
+    {"type_name": "🛷雪橇","type_id": "雪橇教学"},
+    {"type_name": "🥌冰壶","type_id": "冰壶教学"},
+    {"type_name": "🏒冰球","type_id": "冰球教学"},
+    {"type_name": "🥏飞盘","type_id": "飞盘教学"},
+    {"type_name": "⚾棒球","type_id": "棒球教学"},
+    {"type_name": "🥎垒球","type_id": "垒球教学"},
+    {"type_name": "🪀悠悠球","type_id": "悠悠球教学"},
+    {"type_name": "🏈美式橄榄球","type_id": "美式橄榄球教学"},
+    {"type_name": "🏉英式橄榄球","type_id": "英式橄榄球教学"},
+    {"type_name": "⛳高尔夫球","type_id": "高尔夫球教学"},
+    {"type_name": "🎳保龄球","type_id": "保龄球教学"},
+    {"type_name": "🏏板球","type_id": "板球教学"},
+    {"type_name": "🏑曲棍球","type_id": "曲棍球教学"},
+    {"type_name": "🥍袋棍球","type_id": "袋棍球教学"}
+];
+  let filterObj = {
+	"足球教学": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+	"篮球教学": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+	"乒乓球教学": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+	"羽毛球教学": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+	"网球教学": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+	"排球教学": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+	"台球教学": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+	"扔飞镖教学": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+	"拳击教学": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+	"武术教学": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+	"跑步技巧": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+	"放风筝技巧": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+	"钓鱼教学": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+	"游泳教学": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+	"滑雪教学": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+	"滑冰教学": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+	"雪橇教学": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+	"冰壶教学": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+	"冰球教学": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+	"飞盘教学": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+	"棒球教学": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+	"垒球教学": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+	"悠悠球教学": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+	"美式橄榄球教学": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+	"英式橄榄球教学": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+	"高尔夫球教学": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+	"保龄球教学": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+	"板球教学": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+	"曲棍球教学": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}],
+	"袋棍球教学": [{"key": "order","name": "排序","value": [{ "n": "综合排序","v": "0"},{"n": "最多点击","v": "click"},{"n": "最新发布","v": "pubdate"},{"n": "最多弹幕","v": "dm"},{"n": "最多收藏","v": "stow"}]},{"key": "duration","name": "时长","value": [{"n": "全部","v": "0"},{"n": "60分钟以上","v": "4"},{"n": "30~60分钟","v": "3"},{"n": "10~30分钟","v": "2"},{"n": "10分钟以下","v": "1"}]}]
+};
+  return JSON.stringify({
+      class: classes,
+      filters: filterObj,
+  });
+}
+
+async function homeVod() {
+  let html = HOST + '/x/web-interface/search/type?search_type=video&keyword='+homeName;
+  let data = JSON.parse(await request(html)).data.result;
+  let videos = [];
+  data.forEach(function(it) {
+      if(it.bvid!==""){       videos.push({
+          vod_id: it.aid,
+          vod_name: stripHtmlTag(it.title),
+          vod_pic: 'http:'+it.pic,
+          vod_remarks: turnDHM(it.duration) || ''})};
+  });
+  return JSON.stringify({
+      list: videos,
+  });
+}
+
+async function category(tid, pg, filter, extend) {
+  let html = HOST + '/x/web-interface/search/type?search_type=video&page=' + pg + '&keyword=' + (extend.tid || tid) + '&duration=' + (extend.duration || '') + '&order=' + (extend.order || '');
+  let data = JSON.parse(await request(html)).data;
+  let videos = [];
+  data.result.forEach(function(it) {
+      if(it.bvid!==""){       videos.push({
+          vod_id: it.aid,
+          vod_name: stripHtmlTag(it.title),
+          vod_pic: 'https:' + it.pic,
+          vod_remarks: turnDHM(it.duration) || ''})};
+  });
+  return JSON.stringify({
+      page: parseInt(data.page),
+      pagecount: data.numPages,
+      limit: 20,
+      total: data.numResults,
+      list: videos,
+  });
+}
+
+async function detail(id) {
+  let data = JSON.parse(await request(HOST + '/x/web-interface/view?aid=' + id)).data;
+  let vod = {
+      vod_id: data.aid,
+      vod_name: stripHtmlTag(data.title),
+      vod_pic: data.pic,
+      type_name: data.tname,
+      vod_year: new Date(data.pubdate*1000).getFullYear(),
+      vod_remarks: '',
+      vod_director: '[a=cr:' + JSON.stringify({'id':data.owner.name + '_clicklink','name':data.owner.name}) + '/]' + data.owner.name + '[/a]',
+      vod_content: stripHtmlTag(data.desc),
+  };
+  let episodes = data.pages;
+  let playurls = [];
+  episodes.forEach(function(it) {
+    let cid = it.cid;
+    let part = it.part.replace('#', '﹟').replace('$', '﹩');
+    playurls.push(
+        part + '$' + data.aid + '_' + cid
+    )
+  });
+  let playUrl = playurls.join('#');
+  vod.vod_play_from = 'B站';
+  vod.vod_play_url = playUrl;
+  return JSON.stringify({
+    list: [vod],
+  });
+}
+
+async function play(flag, id, flags) {
+  let ids = id.split('_');
+  let html = HOST + '/x/player/playurl?avid=' + ids[0] + '&cid=' + ids[1] + '&qn=116';
+  let data = JSON.parse(await request(html)).data.durl;
+  let maxSize = -1;
+  let position = -1;  const dan = 'https://api.bilibili.com/x/v1/dm/list.so?oid=' + ids[1];
+  data.forEach(function(it, i) {
+      if (maxSize < Number(it.size)) {
+          maxSize = Number(it.size);
+          position = i
+      }
+  });
+  let purl = '';
+  if (data.length > 0) {
+    if (position === -1) {
+        position = 0
+    }
+    purl = data[position].url
+  }
+  // console.debug('我的哔哩 purl =====>' + purl); // js_debug.log
+  return JSON.stringify({
+    parse: 0,
+    url: purl,    danmaku: dan,    
+    header: getMb(),
+  });
+}
+
+async function search(wd, quick, pg) {
+  if(searchable==1){
+  if (pg <= 0 || typeof(pg) == 'undefined') pg = 1;
+  let html = HOST + '/x/web-interface/search/type?search_type=video&keyword=' + wd + '&page=' + pg;
+  let data = JSON.parse(await request(html)).data;
+  let videos = [];
+  data.result.forEach(function(it) {
+    if(it.bvid!==""){       videos.push({
+        vod_id: it.aid,
+        vod_name: stripHtmlTag(it.title),
+        vod_pic: 'https:' + it.pic,
+        vod_remarks: turnDHM(it.duration) || '',
+    })};
+  });
+  return JSON.stringify({
+      page: parseInt(data.page),
+      pagecount: data.numPages,
+      limit: 20,
+      total: data.numResults,
+      list: videos,
+  });
+  }else{
+	  return null;
+  }
+}
+
+function getHeader(cookie) {
+  let header = {};
+  header['cookie'] = cookie;
+  header['User-Agent'] = PC_UA;
+  header['Referer'] = 'https://www.bilibili.com';
+  return header;
+}
+
+function getMb() {
+  return getHeader(cookie);
+}
+
+function stripHtmlTag(src) {
+  return src
+      .replace(/<\/?[^>]+(>|$)/g, '')
+      .replace(/&.{1,5};/g, '')
+      .replace(/\s{2,}/g, ' ');
+}
+
+function turnDHM(duration) {
+  let min = duration.split(':')[0];
+  let sec = duration.split(':')[1];
+  if (min == 0) {
+      return sec + '秒';
+  } else if (0 < min && min < 60) {
+      return min + '分';
+  } else if (60 <= min && min < 1440) {
+      if (min % 60 == 0) {
+          let h = min / 60;
+          return h + '小时';
+      } else {
+          let h = min / 60;
+          h = (h + '').split('.')[0];
+          let m = min % 60;
+          return h + '小时' + m + '分';
+      }
+  } else if (min >= 1440) {
+      let d = min / 60 / 24;
+      d = (d + '').split('.')[0];
+      let h = min / 60 % 24;
+      h = (h + '').split('.')[0];
+      let m = min % 60;
+      let dhm = '';
+      if (d > 0) {
+          dhm = d + '天'
+      }
+      if (h >= 1) {
+          dhm = dhm + h + '小时'
+      }
+      if (m > 0) {
+          dhm = dhm + m + '分'
+      }
+      return dhm;
+  }
+  return null;
+}
+
+export function __jsEvalReturn() {
+  return {
+      init: init,
+      home: home,
+      homeVod: homeVod,
+      category: category,
+      detail: detail,
+      play: play,
+      search: search,
+  };
+}
