b55a8b665f4411ae466b36c63d4c0879815c1906
Cacifer <jack.cacifer@gmail.com>
2024-06-30 14:51:19 +0800

    update

--- JS/1080资源.js
+++ JS/1080资源.js
@@ -1,9 +1,9 @@
 var rule = {
-    title: '乐视资源',
-    host: 'https://leshiapi.com',
-    homeUrl: '/api.php/provide/vod/?ac=detail',
-    searchUrl: '/api.php/provide/vod/?ac=detail&wd=**&pg=fypage',
-    detailUrl: '/api.php/provide/vod/?ac=detail&ids=fyid', //非必填,二级详情拼接链接
+    title: '1080资源',
+    host: 'https://api.1080zyku.com',
+    homeUrl: '/inc/api_mac10.php/?ac=detail',
+    searchUrl: '/inc/api_mac10.php/?ac=detail&wd=**&pg=fypage',
+    detailUrl: '/inc/api_mac10.php/?ac=detail&ids=fyid', //非必填,二级详情拼接链接
     searchable: 2,
     quickSearch: 0,
     filterable: 1,
@@ -12,9 +12,9 @@ var rule = {
     multi: 1,
     timeout: 5000,
     limit: 6,
-    url: '/api.php/provide/vod/?ac=detail&t=fyclass&pg=fypage&f=',
+    url: '/inc/api_mac10.php/?ac=detail&t=fyclass&pg=fypage&f=',
     class_name: '短剧&国产剧&美剧&韩剧&动作片&科幻片&恐怖片&战争片&剧情片&喜剧片&爱情片&国漫&欧美动漫&日韩动漫',
-    class_url: '54&22&04&23&06&08&09&11&10&12&07&31&33&32',
+    class_url: '83&12&15&14&5&8&9&11&10&6&7&66&68&67',
     //推荐: 'json:list;vod_name;vod_pic;vod_remarks;vod_id', // double: true, // 推荐内容是否双层定位
     推荐: '',
     // 一级: 'json:list;vod_name;vod_pic;vod_remarks;vod_id',
@@ -31,7 +31,7 @@ var rule = {
         }
         let d = [];
         // 忽略分类
-        let cate_exclude = '37,48,49,50,51,53';
+        let cate_exclude = '19,61,92,93,94';
         let type_id = getParam(input,'t');
         if(!cate_exclude.match(type_id)){
             let html = request(input);
@@ -71,7 +71,7 @@ var rule = {
     搜索: `js:
         let d = [];
         // 忽略分类
-        let cate_exclude = '37,48,49,50,51,53';
+        let cate_exclude = '19,61,92,93,94';
         let html = request(input);
         let list = JSON.parse(html).list;
         list.forEach(function (it){
